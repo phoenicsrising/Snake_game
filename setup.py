00579045080084309faa9b375ce46df39710a8c8
phoenicsrising <saruno_hatashi@hotmail.com>
2023-03-03 14:53:55 +0200

    Removed setup.py from Git tracking

--- setup.py
+++
@@ -1,9 +0,0 @@
-import cx_Freeze
-
-executables = [cx_Freeze.Executable("main.py")]
-
-cx_Freeze.setup(
-    name="My Snake Game",
-    options={"build_exe": {"packages":["pygame"], "include_files":["snake.py", "food.py", "scoreboard.py"]}},
-    executables=executables
-)
